88ac976bca4d88af7a32b4ba2a9b06f0c2aa0e57
dktaak <dktaak23@gmail.com>
2021-05-02 11:57:40 +0530

    initial commit

---
+++ Objects/rtx_conf_lpc21xx.d
@@ -0,0 +1,4 @@
+.\objects\rtx_conf_lpc21xx.o: RTX_Conf_LPC21xx.c
+.\objects\rtx_conf_lpc21xx.o: D:\arm\ARM\RV31\INC\RTL.h
+.\objects\rtx_conf_lpc21xx.o: D:\arm\ARM\INC\Philips\LPC21xx.H
+.\objects\rtx_conf_lpc21xx.o: D:\arm\ARM\RV31\INC\RTX_lib.c
